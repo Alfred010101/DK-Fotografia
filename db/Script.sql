40cd811a3c979f62ee1d726ef8c39fbc3fb6dd64
Alfred Arista <arista21dominguez@gmail.com>
2025-10-20 23:09:27 -0600

    registros de prueba

--- db/Script.sql
+++ db/Script.sql
@@ -14,9 +14,16 @@ CREATE TABLE IF NOT EXISTS dk_fotografia.user(
 
 INSERT INTO dk_fotografia.user (first_name, last_name, username, password, role)
 	VALUES
-		('Alfredo', 'Arista', 'alfredo_admin', '$2a$10$H1Q4oF1zV9sCw1m5Wc4A7uPzRsn6DkLrYv9h9z0V3iTkZC1S4s2sC', 'ADMIN'),
-		('María', 'López', 'mlopez', '$2a$10$7R7pO6Ybz0g9gF4sE5zv4OSd7f3KSh7cR7Tt5W5mQ9mE4qUuBf2bC', 'CUSTOMER'),
-		('Juan', 'Pérez', 'jperez', '$2a$10$Xc2OiyQ7U3A5GvVf6F3XgOg3E6Ff2v8WkHk1dZcB4wA1nZfD5aKzC', 'CUSTOMER'),
-		('Lucía', 'Martínez', 'luciam', '$2a$10$KkZ1zTnPp4Rj5E8gTzE5hO3YjT6Wf3RjLzA6nG2rP5zH2dWvQ4aV.', 'CUSTOMER');
+		('Alfredo', 'Arista', 'alfredo_admin', '$2a$10$XLnYmx/dV8f6ucgZpzDwE.PlvHW6CoB2duTzKcg0AZKDjLkXek0Fu', 'ADMIN'),
+		('María', 'López', 'mlopez', '$2a$10$XLnYmx/dV8f6ucgZpzDwE.PlvHW6CoB2duTzKcg0AZKDjLkXek0Fu', 'CUSTOMER'),
+		('Juan', 'Pérez', 'jperez', '$2a$10$XLnYmx/dV8f6ucgZpzDwE.PlvHW6CoB2duTzKcg0AZKDjLkXek0Fu', 'CUSTOMER'),
+		('Lucía', 'Martínez', 'luciam', '$2a$10$XLnYmx/dV8f6ucgZpzDwE.PlvHW6CoB2duTzKcg0AZKDjLkXek0Fu', 'CUSTOMER');
 
 SELECT * FROM dk_fotografia.`user`;
+
+#DROP DATABASE dk_fotografia;
+
+
+
+
+
